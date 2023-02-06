8b9bbcf26ab8fe23828b91b38d05a441ae167320
Yauhen Varapayeu <35904041+AmidaWolf@users.noreply.github.com>
2023-02-06 20:56:40 +0300

    feat: add track module

--- src/track/track.interface.ts
+++ src/track/track.interface.ts
@@ -1,7 +1,46 @@
-interface Track {
+import { IsNumber, IsString, ValidateIf } from 'class-validator';
+
+export class Track {
+  @IsString()
   id: string; // uuid v4
+
+  @IsString()
   name: string;
+
+  @ValidateIf((object, value) => value !== null)
   artistId: string | null; // refers to Artist
+
+  @ValidateIf((object, value) => value !== null)
   albumId: string | null; // refers to Album
+
+  @IsNumber()
   duration: number; // integer number
+}
+
+export class CreateTrackDto {
+  @IsString()
+  name: string;
+
+  @ValidateIf((object, value) => value !== null)
+  artistId: string | null; // refers to Artist
+
+  @ValidateIf((object, value) => value !== null)
+  albumId: string | null; // refers to Album
+
+  @IsNumber()
+  duration: number; // integer number
+}
+
+export class UpdateTrackDto {
+  @IsString()
+  name?: string;
+
+  @ValidateIf((object, value) => value !== null)
+  artistId?: string | null; // refers to Artist
+
+  @ValidateIf((object, value) => value !== null)
+  albumId?: string | null; // refers to Album
+
+  @IsNumber()
+  duration?: number; // integer number
 }
--- src/track/track.service.ts
+++ src/track/track.service.ts
@@ -1,4 +1,93 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { CreateTrackDto, Track, UpdateTrackDto } from './track.interface';
+import { isUUID } from 'class-validator';
+import { v4 as uuidv4 } from 'uuid';
 
 @Injectable()
-export class TrackService {}
+export class TrackService {
+  private readonly tracks: Track[] = [];
+
+  private validateId(id: string) {
+    if (!isUUID(id)) {
+      throw new BadRequestException(`Track id is not uuid v4 format`);
+    }
+  }
+
+  getAllTracks(): Track[] {
+    return this.tracks;
+  }
+
+  findTrackById(id: string): Track {
+    this.validateId(id);
+
+    const foundTrackIndex = this.tracks.findIndex((user) => user.id === id);
+
+    if (foundTrackIndex === -1) {
+      throw new NotFoundException(`Track with id ${id} not found.`);
+    }
+
+    return this.tracks[foundTrackIndex];
+  }
+
+  createTrack(track: CreateTrackDto): Track {
+    const newTrack: Track = {
+      id: uuidv4(),
+      name: track.name,
+      artistId: track.artistId,
+      albumId: track.albumId,
+      duration: track.duration,
+    };
+
+    this.tracks.push(newTrack);
+
+    return newTrack;
+  }
+
+  updateTrack(id: string, updateTrackDto: UpdateTrackDto): Track {
+    this.validateId(id);
+
+    const foundTrackIndex = this.tracks.findIndex((user) => user.id === id);
+
+    if (foundTrackIndex === -1) {
+      throw new NotFoundException(`Track with id ${id} not found.`);
+    }
+
+    const foundTrack = this.tracks[foundTrackIndex];
+
+    if (updateTrackDto.albumId) {
+      foundTrack.albumId = updateTrackDto.albumId;
+    }
+
+    if (updateTrackDto.name) {
+      foundTrack.name = updateTrackDto.name;
+    }
+
+    if (updateTrackDto.artistId) {
+      foundTrack.artistId = updateTrackDto.artistId;
+    }
+
+    if (updateTrackDto.duration) {
+      foundTrack.duration = updateTrackDto.duration;
+    }
+
+    this.tracks[foundTrackIndex] = foundTrack;
+
+    return this.tracks[foundTrackIndex];
+  }
+
+  removeTrack(id: string) {
+    this.validateId(id);
+
+    const foundUserIndex = this.tracks.findIndex((user) => user.id === id);
+
+    if (foundUserIndex === -1) {
+      throw new NotFoundException(`Track with id ${id} not found.`);
+    }
+
+    this.tracks.splice(foundUserIndex, 1);
+  }
+}
