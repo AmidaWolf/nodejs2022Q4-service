45b2fde7fa854e4beb1b851323d956dbef2fc99b
Yauhen Varapayeu <35904041+AmidaWolf@users.noreply.github.com>
2023-02-06 19:40:21 +0300

    feat: add user module, add basic interfaces
    all tests passed for user

---
+++ src/user/user.interface.ts
@@ -0,0 +1,45 @@
+import { IsNumber, IsString } from 'class-validator';
+
+export class User {
+  @IsString()
+  id?: string; // uuid v4
+
+  @IsString()
+  login: string;
+
+  @IsString()
+  password: string;
+
+  @IsNumber()
+  version: number; // integer number, increments on update
+
+  @IsNumber()
+  createdAt: number; // timestamp of creation
+
+  @IsNumber()
+  updatedAt?: number; // timestamp of last update
+}
+
+export class UserResponse {
+  id: string;
+  login: string;
+  version: number;
+  createdAt: number;
+  updatedAt?: number;
+}
+
+export class CreateUserDto {
+  @IsString()
+  login: string;
+
+  @IsString()
+  password: string;
+}
+
+export class UpdatePasswordDto {
+  @IsString()
+  oldPassword: string; // previous password
+
+  @IsString()
+  newPassword: string; // new password
+}
